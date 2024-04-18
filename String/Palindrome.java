8845e38c8455a7bde3d1d004f2757f08b770b6dc
Uttpal Kant <uttpal61@gmail.com>
2024-04-18 14:07:33 +0530

    file added

--- String/Palindrome.java
+++ String/Palindrome.java
@@ -3,6 +3,7 @@ package String;
 public class Palindrome {
     //Function to get palindrome
     public static boolean isPalindrome(String str){
+        // hii
         for(int i=0;i<str.length()/2;i++){ // time complexcity is O(n)!
             int n= str.length();
             if(str.charAt(i) != str.charAt(n-1-i) ){
